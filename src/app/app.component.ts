0365e3af8c6a15060b4c90710d82c0deeabb892e
jesse <jsoldat@hotmail.com>
2017-06-12 20:57:31 -0400

    first animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'pink',
+        width: '100px',
+        height: '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'green',
+        width: '500px',
+        height: '300px'
+      })),
+      transition('default <=> clicked', animate('1500ms 500ms linear'))
+    ])
+    
+    
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  clickInfo = 'default';
+
+  onToggleClickInfo() {
+    this.clickInfo = this.clickInfo === 'default' ? 'clicked' : 'default';
+  }
+ 
 }
