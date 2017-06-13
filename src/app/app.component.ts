c4ae2fce6ddbb42803f6e235c8d6f2404270bfd1
jesse <jsoldat@hotmail.com>
2017-06-13 10:03:10 -0400

    moved animations to their own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { routerTransition } from './animations/slide-in.animation';
+import { clickedStateTrigger, numberEnteredStateTrigger } from './animations/basic-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // animations: [routerTransition()],
+ 	// host: { '[@routerTransition]': '' }
+  
   animations: [
-    trigger('clickedState', [
-      state('default', style({
-        backgroundColor: 'pink',
-        width: '100px',
-        height: '100px',
-        borderRadius: '0',
-        border: '5px solid green'
-      })),
-      state('clicked', style({
-        backgroundColor: 'green',
-        width: '300px',
-        height: '300px',
-        borderRadius: '150px',
-        border: '15px solid pink'        
-      })),
-      state('mousedown', style({
-        width: '100px',
-        height: '100px',
-        borderRadius: '0',
-        backgroundColor: 'purple',
-        border: '3px solid black'
-      })),
-      
-      transition('default <=> clicked', animate('1500ms 500ms linear')),
-      transition('clicked <=> mousedown', animate(500))
-    ])   
+    clickedStateTrigger,
+    numberEnteredStateTrigger
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   clickInfo = 'default';
+  numbers: Array<number> = Array.from(Array(10).keys());
+  numberEntered: number = 0;
+
+  ngOnInit() {
+    // console.log(this.numbers);
+    
+  }
 
   onToggleClickInfo() {
     this.clickInfo = 'clicked';
