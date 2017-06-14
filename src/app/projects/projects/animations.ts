59a1c5131eb8fad5d3933ab0e07c76fb6d403ec8
jesse <jsoldat@hotmail.com>
2017-06-14 18:56:13 -0400

    list is staggered in

--- src/app/projects/projects/animations.ts
+++ src/app/projects/projects/animations.ts
@@ -1,4 +1,4 @@
-import { style, state, transition, animate, trigger } from '@angular/animations';
+import { style, state, transition, animate, trigger, keyframes } from '@angular/animations';
 export const markedTrigger = trigger('markedState', [
   state('default', style({
     border: '1px solid black',
@@ -27,5 +27,39 @@ export const markedTrigger = trigger('markedState', [
       padding: '20px'
     }),
     animate('300ms ease-out')
+  ])
+]);
+
+export const itemStateTrigger = trigger('itemState', [
+  transition(':enter', [
+    animate('800ms ease-out', keyframes([
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+        offset: 0
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(15%)',
+        offset: 0.5
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(0)',
+        offset: 1
+      })
+    ]))
+  ]),
+  transition(':leave', [
+    animate('800ms ease-in', keyframes([
+      style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      }),
+      style({
+        opacity: 0,
+        transform: 'translateX(100%)'
+      })
+    ]))
   ])
 ])
--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from './../projects.service';
 import { Project } from './../project.model';
-
-import { markedTrigger } from './animations';
-
+import { AnimationEvent } from '@angular/animations';
+import { markedTrigger, itemStateTrigger } from './animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
   animations: [ 
-    markedTrigger
+    markedTrigger,
+    itemStateTrigger
   ]
 })
 export class ProjectsComponent implements OnInit {
   createNew: boolean = false;
+  displayedProjects: Project[] = [];
   projects: Project[];
   progress: string = 'progressing';
   markedProjectIndex: number = 0; 
@@ -26,8 +27,21 @@ export class ProjectsComponent implements OnInit {
         (projects: Project[]) => {
           this.progress = 'finished';
           this.projects = projects;  
-                 
+          if(this.projects.length >=1 ) {
+            this.displayedProjects.push(this.projects[0])
+          }                 
       });
+  }
+  onItemAnimated(animationEvent: AnimationEvent, projectId: number) {
+    if(animationEvent.fromState != 'void') {
+      return;
+    }
+    if(this.projects.length > projectId + 1) {
+      this.displayedProjects.push(this.projects[projectId +1 ]);
+    } else {
+      this.projects = this.displayedProjects;
+    }
+    
   }
   onStatusUpdated(newStatus: string, id: number) {
     this.projects[id].status = newStatus;
